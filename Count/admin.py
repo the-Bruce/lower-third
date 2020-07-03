a45837cd5f0bfcbe84e6e089c64346bf1ab28745
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2020-07-03 13:54:38 +0100

    Update count admin

--- Count/admin.py
+++ Count/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import DayCount
 
 class DayCountAdmin(admin.ModelAdmin):
-    list_display = ('date',)
+    list_display = ('date','peak','total')
     list_display_links = ('date',)
-    readonly_fields = ('peak','date')
+    readonly_fields = ('peak','date','total')
 
 # Register your models here.
 admin.site.register(DayCount, DayCountAdmin)

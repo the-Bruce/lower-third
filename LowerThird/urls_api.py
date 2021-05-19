5cbd781b7fef3ef92fef93b2c8dad9539c43c007
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2021-05-19 15:36:33 +0100

    Add new rss views to lower third

--- LowerThird/urls_api.py
+++ LowerThird/urls_api.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import CurrentState, UpdateState
+from .views import CurrentState, UpdateState, CurrentStateRSS, CurrentStateListRSS
 urlpatterns = [
     path('session_state/', CurrentState.as_view()),
     path('update/', UpdateState.as_view()),
+    path('session_state/rss/<slug:session>/', CurrentStateRSS()),
+    path('session_state/rss/<slug:session>/all/', CurrentStateListRSS())
+
 ]
